af8cadd82dd9447a80e65990f8466694da2c9347
tshletsw <tshegoletswalo26@gmail.com>
2025-09-26 12:57:53 +0200

    day05

---
+++ day05/day05.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class day05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <= 10; i++){
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        bufferedReader.close();
+    }
+}
