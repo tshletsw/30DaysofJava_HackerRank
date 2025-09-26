b6fbe0cfc33acd9d759cf00caa8d1f3247dc4188
tshletsw <tshegoletswalo26@gmail.com>
2025-09-26 10:50:36 +0200

    day02

---
+++ day02/day02.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+     double tip = meal_cost/100. * tip_percent;
+     double tax = tax_percent/100. * meal_cost;
+     double total = meal_cost + tip + tax;
+     total = Math.round(total);
+     int  newtotal = (int) total;
+     System.out.println(newtotal);
+    }
+
+}
+
+public class day02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
